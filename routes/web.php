7d6ad1d156b7901cedf385de3b71992a2cb57df1
marinodilauro <m.dilauro90@gmail.com>
2024-07-08 16:49:58 +0200

    Add dashboard controller for admin and user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\User\UserDashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +20,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
+
+Route::middleware(['auth', 'verified'])
+    ->name('user.')
+    ->prefix('user')
+    ->group(function () {
+        // Put all the routes that needs to be protected by our authentication system
+        // All routes needs to share a common name and prefix and the middleware
+
+        Route::get('/', [UserDashboardController::class, 'index'])->name('dashboard'); // /user.dashboard
+    });
+
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        // Put all the routes that needs to be protected by our authentication system
+        // All routes needs to share a common name and prefix and the middleware
+
+        Route::get('/', [AdminDashboardController::class, 'index'])->name('dashboard'); // /admin.dashboard
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +50,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
